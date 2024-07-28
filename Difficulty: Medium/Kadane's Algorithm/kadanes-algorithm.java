dcfdea5c5e9abf3fb7c6e314d853c711524dc0a0
Devanshi Prasad <102360503+devanshiprasad@users.noreply.github.com>
2024-07-28 17:52:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -50,10 +50,12 @@ class Solution {
 
         for (int i = 0; i < size; i++) {
             max_ending_here = max_ending_here + arr[i];
-            if (max_so_far < max_ending_here)
-                max_so_far = max_ending_here;
-            if (max_ending_here < 0)
-                max_ending_here = 0;
+            if (max_so_far < max_ending_here){
+                max_so_far = max_ending_here;}
+            if (max_ending_here < 0){
+                 max_ending_here = 0;
+            }
+               
         }
         return max_so_far;
         }
