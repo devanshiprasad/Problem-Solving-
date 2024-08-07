b43615dc2bb0bc80311700301cedf9c7af145d43
Devanshi Prasad <102360503+devanshiprasad@users.noreply.github.com>
2024-08-07 12:42:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        StringBuilder sb = new StringBuilder();
+        for (int e : a) sb.append(e + " ");
+        System.out.println(sb);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String str[] = br.readLine().trim().split(" ");
+
+            int n = Integer.parseInt(str[0]);
+            int s = Integer.parseInt(str[1]);
+
+            int[] arr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(arr, n, s);
+
+            IntArray.print(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static ArrayList<Integer> subarraySum(int[] arr, int n, int s) {
+        // code here
+        int start=0;
+        int curr=0;
+        ArrayList<Integer> a= new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            curr+= arr[i];
+            
+            while(curr>s && start<i){
+                curr-=arr[start];
+                start++;
+            }
+            
+            if(curr==s){
+                a.add(start+1);
+                a.add(i+1);
+                return a;
+            }
+        }
+        if(a.isEmpty()) a.add(-1);
+        return a;
+        
+    }
+}
