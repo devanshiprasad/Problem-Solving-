55059f72dd0a0d8e43b261407fd9c8d784dd548a
Devanshi Prasad <102360503+devanshiprasad@users.noreply.github.com>
2024-11-18 02:01:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Detect Loop in linked list/detect-loop-in-linked-list.java
+++ Difficulty: Medium/Detect Loop in linked list/detect-loop-in-linked-list.java
@@ -59,7 +59,9 @@ class GFG {
                 System.out.println("true");
             else
                 System.out.println("false");
-        }
+        
+System.out.println("~");
+}
     }
 }
 
@@ -83,16 +85,17 @@ class Solution {
     // Function to check if the linked list has a loop.
     public static boolean detectLoop(Node head) {
         // Add code here
-        Node fast=head;
         Node slow=head;
+        Node fast= head;
         while(fast!=null && fast.next!=null){
             slow=slow.next;
-            fast= fast.next.next;
+            fast=fast.next.next;
             
             if(slow==fast){
                 return true;
             }
         }
         return false;
+        
     }
 }
